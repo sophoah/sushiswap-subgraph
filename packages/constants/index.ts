dc6f56e4e1ad5c39a72f53d8ba4e1300749b1314
theopenfinanceproject <grosjean.alexandre@gmail.com>
2021-05-09 14:02:51 +0200

    yes

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -24,17 +24,17 @@ export const BIG_INT_ZERO = BigInt.fromI32(0)
 
 export const LOCKUP_POOL_NUMBER = BigInt.fromI32(29)
 
-export const FACTORY_ADDRESS = Address.fromString('0xc35dadb65012ec5796536bd9864ed8773abc74c4')
+export const FACTORY_ADDRESS = Address.fromString('0xBdEBd3af1723BBB33A8aDB68e3e77d65D7266F59')
 
 export const LOCKUP_BLOCK_NUMBER = BigInt.fromI32(10959148)
 
-export const MASTER_CHEF_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
+export const MASTER_CHEF_ADDRESS = Address.fromString('0xaC71B617a58B3CC136D1f6A118252f331faB44fC')
 
 export const SUSHI_BAR_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
 
-export const SUSHI_MAKER_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
+export const SUSHI_MAKER_ADDRESS = Address.fromString('0x7954dA0b4A81019A9313403FCDe072B93Aa41d36')
 
-export const SUSHI_TOKEN_ADDRESS = Address.fromString('0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a')
+export const SUSHI_TOKEN_ADDRESS = Address.fromString('0xc0431Ddcc0D213Bf27EcEcA8C2362c0d0208c6DC')
 
 export const SUSHI_USDT_PAIR_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
 
@@ -46,11 +46,11 @@ export const SUSHI_DISTRIBUTOR_ADDRESS = Address.fromString('0x00000000000000000
 
 export const NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
 
-export const USDC_WETH_PAIR = '0x853ee4b2a13f8a742d64c8f088be7ba2131f670d'
+export const USDC_WETH_PAIR = '0xF47F0cc859B78805A2852c744Bc1fb6A676aF087'
 
-export const DAI_WETH_PAIR = '0x4a35582a710e1f4b2030a3f826da20bfb6703c09'
+export const DAI_WETH_PAIR = '0x0000000000000000000000000000000000000000'
 
-export const USDT_WETH_PAIR = '0xf6422b997c7f54d1c6a6e103bcb1499eea0a7046'
+export const USDT_WETH_PAIR = '0xF880c5D048696d80425C183b3f77b8693d0B34D5'
 
 export const SUSHI_USDT_PAIR = '0x0000000000000000000000000000000000000000'
 
@@ -60,11 +60,11 @@ export const MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
 // minimum liquidity for price to get tracked
 export const MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('5')
 
-export const WETH_ADDRESS = Address.fromString('0x7ceb23fd6bc0add59e62ac25578270cff1b9f619')
+export const WETH_ADDRESS = Address.fromString('0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a')
 
 export const SUSHISWAP_WETH_USDT_PAIR_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
 
-export const USDT_ADDRESS = Address.fromString('0xc2132d05d31c914a87c6611c10748aeb04b58e8f')
+export const USDT_ADDRESS = Address.fromString('0x0aB43550A6915F9f67d0c454C2E90385E6497EaA')
 
 export const MASTER_CHEF_START_BLOCK = BigInt.fromI32(10750000)
 
@@ -78,39 +78,3 @@ export const UNISWAP_SUSHI_ETH_PAIR_ADDRESS = Address.fromString('0x000000000000
 
 export const UNISWAP_SUSHI_USDT_PAIR_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
 
-
-// Bentobox constants
-export const BENTOBOX_ADDRESS = Address.fromString('0xf5bce5077908a1b7370b9ae04adc565ebd643966')
-
-export const KASHI_PAIR_MEDIUM_RISK_MASTER_ADDRESS = Address.fromString('0x2cba6ab6574646badc84f0544d05059e57a5dc42')
-
-export const BENTOBOX_DEPOSIT = 'deposit'
-
-export const BENTOBOX_TRANSFER = 'transfer'
-
-export const BENTOBOX_WITHDRAW = 'withdraw'
-
-export const KASHI_PAIR_MEDIUM_RISK_TYPE = 'medium'
-
-export const PAIR_ADD_COLLATERAL = 'addCollateral'
-
-export const PAIR_REMOVE_COLLATERAL = 'removeCollateral'
-
-export const PAIR_ADD_ASSET = 'addAsset'
-
-export const PAIR_REMOVE_ASSET = 'removeAsset'
-
-export const PAIR_BORROW = 'borrow'
-
-export const PAIR_REPAY = 'repay'
-
-
-// MiniChef
-export const MINI_CHEF_ADDRESS = Address.fromString('0x0769fd68dfb93167989c6f7254cd0d766fb2841f')
-
-export const ACC_SUSHI_PRECISION = BigInt.fromString('1000000000000')
-
-// Matic Complex Rewarder (note: putting here for now since we don't need to fill in every config file with this address)
-export const MATIC_COMPLEX_REWARDER = Address.fromString('0xa3378ca78633b3b9b2255eaa26748770211163ae')
-
-export const WMATIC_ADDRESS = Address.fromString('0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270')
