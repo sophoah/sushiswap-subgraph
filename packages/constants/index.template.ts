13c8ec2d20838a5e7bc5aceb25b3c2bbae1f889a
theopenfinanceproject <grosjean.alexandre@gmail.com>
2021-05-09 14:46:43 +0200

    yes again

--- packages/constants/index.template.ts
+++ packages/constants/index.template.ts
@@ -78,39 +78,3 @@ export const UNISWAP_SUSHI_ETH_PAIR_ADDRESS = Address.fromString('{{ uniswap_sus
 
 export const UNISWAP_SUSHI_USDT_PAIR_ADDRESS = Address.fromString('{{ uniswap_sushi_usdt_pair_address }}')
 
-
-// Bentobox constants
-export const BENTOBOX_ADDRESS = Address.fromString('{{ bentobox_address }}')
-
-export const KASHI_PAIR_MEDIUM_RISK_MASTER_ADDRESS = Address.fromString('{{ kashi_pair_medium_risk_master_address }}')
-
-export const BENTOBOX_DEPOSIT = 'deposit'
-
-export const BENTOBOX_TRANSFER = 'transfer'
-
-export const BENTOBOX_WITHDRAW = 'withdraw'
-
-export const KASHI_PAIR_MEDIUM_RISK_TYPE = 'medium'
-
-export const PAIR_ADD_COLLATERAL = 'addCollateral'
-
-export const PAIR_REMOVE_COLLATERAL = 'removeCollateral'
-
-export const PAIR_ADD_ASSET = 'addAsset'
-
-export const PAIR_REMOVE_ASSET = 'removeAsset'
-
-export const PAIR_BORROW = 'borrow'
-
-export const PAIR_REPAY = 'repay'
-
-
-// MiniChef
-export const MINI_CHEF_ADDRESS = Address.fromString('{{ minichef_address }}')
-
-export const ACC_SUSHI_PRECISION = BigInt.fromString('1000000000000')
-
-// Matic Complex Rewarder (note: putting here for now since we don't need to fill in every config file with this address)
-export const MATIC_COMPLEX_REWARDER = Address.fromString('0xa3378ca78633b3b9b2255eaa26748770211163ae')
-
-export const WMATIC_ADDRESS = Address.fromString('0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270')
